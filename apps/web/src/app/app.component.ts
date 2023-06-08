eb33481a98229d0dc531522e170dd72e928b365f
Philipp Bauch <philippbauch@outlook.com>
2023-06-08 13:35:55 +0200

    Add AuthService

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { AuthService } from './services/auth.service';
+
 @Component({
   standalone: true,
   imports: [RouterModule],
@@ -8,4 +10,10 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private readonly authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.restore();
+  }
+}
---
+++ apps/web/src/app/services/auth.service.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+import {
+  BehaviorSubject,
+  Observable,
+  distinctUntilChanged,
+  map,
+  tap,
+} from 'rxjs';
+
+import { JwtPayload, Role } from '@bkr/api-interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly _token$ = new BehaviorSubject<string | null>(null);
+  readonly token$ = this._token$.pipe(distinctUntilChanged());
+
+  /**
+   * Observable that emits information about the current user.
+   * Emits `null`, if no user is logged in.
+   */
+  readonly user$ = this.token$.pipe(
+    map((token) => (token !== null ? jwtDecode<JwtPayload>(token) : null))
+  );
+
+  readonly isAdmin$ = this.user$.pipe(
+    map((user) => user?.role === Role.ADMIN),
+    distinctUntilChanged()
+  );
+  readonly isStation$ = this.user$.pipe(
+    map((user) => user?.role === Role.STATION),
+    distinctUntilChanged()
+  );
+
+  constructor(private readonly http: HttpClient) {}
+
+  login(code: string): Observable<string> {
+    return this.http
+      .post<{ token: string }>('/token', {
+        code: code,
+      })
+      .pipe(
+        map((res) => res.token),
+        tap((token) => this._token$.next(token)),
+        tap((token) => localStorage.setItem('token', token))
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this._token$.next(null);
+  }
+
+  restore() {
+    const token = localStorage.getItem('token');
+    this._token$.next(token);
+  }
+}
