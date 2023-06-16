275fc1d223be811d42faa3b1a0ac1403f851a58b
Philipp Bauch <philippbauch@outlook.com>
2023-06-16 21:12:36 +0200

    Send a static payload to create a new team

--- apps/web/src/app/pages/team-new/team-new.component.ts
+++ apps/web/src/app/pages/team-new/team-new.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormGroup } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 import { ButtonComponent, InputDirective } from '../../components';
-import { TeamService } from '../../services';
+import { NotificationService, TeamService } from '../../services';
 
 @Component({
   selector: 'bkr-team-new',
@@ -13,5 +15,38 @@ import { TeamService } from '../../services';
   styleUrls: ['./team-new.component.scss'],
 })
 export class TeamNewComponent {
-  constructor(private readonly teamService: TeamService) {}
+  form = new FormGroup({});
+  loading = false;
+
+  private readonly destroyRef = inject(DestroyRef);
+
+  constructor(
+    private readonly notificationService: NotificationService,
+    private readonly router: Router,
+    private readonly teamService: TeamService
+  ) {}
+
+  handleSave(): void {
+    this.loading = true;
+
+    this.teamService
+      .createTeam({
+        name: 'Team A',
+        number: 1,
+        members: ['Alice', 'Bob'],
+      })
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: () => {
+          this.loading = false;
+          this.notificationService.success('Team wurde erstellt.');
+
+          this.router.navigate(['/teams']);
+        },
+        error: () => {
+          this.loading = false;
+          this.notificationService.error('Team konnte nicht erstellt werden.');
+        },
+      });
+  }
 }
