d06aff6f02a28d89ca73e0d0382def89af99e9ad
Philipp Bauch <philippbauch@outlook.com>
2023-06-16 21:31:49 +0200

    Create a team without members

--- apps/web/src/app/pages/team-new/team-new.component.ts
+++ apps/web/src/app/pages/team-new/team-new.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, DestroyRef, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FormGroup } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
 import { ButtonComponent, InputDirective } from '../../components';
@@ -10,15 +16,34 @@ import { NotificationService, TeamService } from '../../services';
 @Component({
   selector: 'bkr-team-new',
   standalone: true,
-  imports: [ButtonComponent, CommonModule, InputDirective, RouterModule],
+  imports: [
+    ButtonComponent,
+    CommonModule,
+    InputDirective,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './team-new.component.html',
   styleUrls: ['./team-new.component.scss'],
 })
 export class TeamNewComponent {
-  form = new FormGroup({});
+  form = new FormGroup({
+    name: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    number: new FormControl<number | null>(null, {
+      validators: [Validators.required],
+    }),
+    members: new FormArray([this.buildMemberControl()]),
+  });
   loading = false;
 
   private readonly destroyRef = inject(DestroyRef);
+
+  get members(): FormArray<FormControl<string>> {
+    return this.form.controls['members'];
+  }
 
   constructor(
     private readonly notificationService: NotificationService,
@@ -26,14 +51,36 @@ export class TeamNewComponent {
     private readonly teamService: TeamService
   ) {}
 
+  handleAddScope(): void {
+    this.members.push(this.buildMemberControl());
+  }
+
+  handleRemoveScopeAt(index: number): void {
+    this.members.removeAt(index);
+  }
+
   handleSave(): void {
+    const { name, number, members } = this.form.value;
+
+    if (
+      this.form.invalid ||
+      typeof name === 'undefined' ||
+      typeof number === 'undefined' ||
+      number === null
+    ) {
+      return;
+    }
+
+    const nonEmptyMembers =
+      members?.filter((member) => member.length > 0) ?? [];
+
     this.loading = true;
 
     this.teamService
       .createTeam({
-        name: 'Team A',
-        number: 1,
-        members: ['Alice', 'Bob'],
+        name,
+        number,
+        members: nonEmptyMembers,
       })
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
@@ -48,5 +95,11 @@ export class TeamNewComponent {
           this.notificationService.error('Team konnte nicht erstellt werden.');
         },
       });
+  }
+
+  private buildMemberControl(): FormControl<string> {
+    return new FormControl<string>('', {
+      nonNullable: true,
+    });
   }
 }
