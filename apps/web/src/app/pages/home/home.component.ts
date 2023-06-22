124d74e4711d91a22cff0d4a00d0e0e5c35ec831
Philipp Bauch <philippbauch@outlook.com>
2023-06-22 10:29:56 +0200

    Add a first version of the leaderbord to the home page

--- apps/web/src/app/pages/home/home.component.ts
+++ apps/web/src/app/pages/home/home.component.ts
@@ -3,18 +3,35 @@ import { Component } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterModule } from '@angular/router';
 
-import { ButtonComponent } from '../../components';
-import { AuthService } from '../../services';
+import { Team } from '@bkr/api-interface';
+
+import {
+  ButtonComponent,
+  EmptyComponent,
+  LoadingComponent,
+} from '../../components';
+import { AuthService, TeamService } from '../../services';
 
 @Component({
   selector: 'bkr-home',
   standalone: true,
-  imports: [ButtonComponent, CommonModule, RouterModule],
+  imports: [
+    ButtonComponent,
+    CommonModule,
+    EmptyComponent,
+    LoadingComponent,
+    RouterModule,
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   isStation = toSignal(this.authService.isStation$);
+  loading = toSignal(this.teamService.loading$, { initialValue: false });
+  teams = toSignal(this.teamService.teams$, { initialValue: [] as Team[] });
 
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly teamService: TeamService
+  ) {}
 }
