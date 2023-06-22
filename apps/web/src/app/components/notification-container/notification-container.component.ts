d80410cab6329056214c05f8822eeeef70780fed
Philipp Bauch <philippbauch@outlook.com>
2023-06-22 10:19:39 +0200

    Simplify notifiations container

--- apps/web/src/app/components/notification-container/notification-container.component.ts
+++ apps/web/src/app/components/notification-container/notification-container.component.ts
@@ -1,24 +1,21 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component, DestroyRef, OnInit, inject } from '@angular/core';
-import { HostBinding } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Component, HostBinding } from '@angular/core';
 
-import { MediaService, Notification, ScreenSize } from '../../services';
+import { Notification } from '../../services';
 import { NotificationComponent } from '../notification/notification.component';
 
 const notificationMotion = trigger('notificationMotion', [
   transition(':enter', [
     style({
       opacity: 0,
-      transform:
-        'translateX({{enterTranslateXFrom}}rem) translateY({{enterTranslateYFrom}}rem)',
+      transform: 'translateX(0rem) translateY(0.5rem)',
     }),
     animate(
       '150ms ease-out',
       style({
         opacity: 1,
-        transform: 'translateX(0px) translateY(0px)',
+        transform: 'translateX(0) translateY(0)',
       })
     ),
   ]),
@@ -33,35 +30,10 @@ const notificationMotion = trigger('notificationMotion', [
   templateUrl: './notification-container.component.html',
   styleUrls: ['./notification-container.component.scss'],
 })
-export class NotificationContainerComponent implements OnInit {
+export class NotificationContainerComponent {
   @HostBinding('attr.aria-live') readonly ariaLive = 'assertive';
 
   notifications: Notification[] = [];
-
-  private readonly destroyRef = inject(DestroyRef);
-
-  private screenSize: ScreenSize = ScreenSize.XS;
-
-  get enterTranslateXFrom(): number {
-    return this.screenSize >= ScreenSize.SM ? 0.5 : 0;
-  }
-
-  get enterTranslateYFrom(): number {
-    return this.screenSize >= ScreenSize.SM ? 0 : 0.5;
-  }
-
-  constructor(private readonly mediaService: MediaService) {}
-
-  /**
-   * @implements {OnInit}
-   */
-  ngOnInit(): void {
-    this.mediaService.screenSize$
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((screenSize) => {
-        this.screenSize = screenSize;
-      });
-  }
 
   addNotification(notification: Notification): void {
     this.notifications.unshift(notification);
