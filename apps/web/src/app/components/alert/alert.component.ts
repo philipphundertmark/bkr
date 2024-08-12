8baae9c87735690594198a47a114b12eff51aa37
Philipp Bauch <21973908+philippbauch@users.noreply.github.com>
2024-08-12 20:34:40 +0200

    Update the UI of alerts; and divider to my-station page

--- apps/web/src/app/components/alert/alert.component.ts
+++ apps/web/src/app/components/alert/alert.component.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { InformationCircleIconComponent } from '../../icons/mini';
-
 @Component({
   selector: 'bkr-alert',
   standalone: true,
-  imports: [CommonModule, InformationCircleIconComponent],
+  imports: [CommonModule],
+  styleUrl: './alert.component.scss',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AlertComponent {}
